d0141b494e7b8a0e1d665461212c6e4c9fd69a27
Eli Sipos <elijsipos@gmail.com>
2025-08-19 11:24:34 -0500

    ExerciseDataAccess theoretically complete, have yet to test

--- app/src/main/java/com/example/gymapp/ExerciseDataAccess.java
+++ app/src/main/java/com/example/gymapp/ExerciseDataAccess.java
@@ -1,7 +1,15 @@
 package com.example.gymapp;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.example.gymapp.models.Exercise;
+import com.example.gymapp.models.Exercise;
+import com.example.gymapp.models.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExerciseDataAccess {
     private SQLiteDatabase db;
@@ -23,5 +31,67 @@ public class ExerciseDataAccess {
         db.close();
 
         return id;
+    }
+
+    public List<Exercise> getExercises(){
+        List<Exercise> exercises = new ArrayList<>();
+        String[] cols = new String[]{"id", "name"};
+
+        Cursor cursor = db.query("Exercise", cols,
+                null, null, null, null, null);
+
+        while(cursor.moveToNext()) {
+            long id = cursor.getLong(cursor.getColumnIndexOrThrow("id"));
+            String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+            exercises.add(new Exercise(id, name));
+        }
+        cursor.close();
+        db.close();
+
+        return exercises;
+    }
+
+    public Exercise getExerciseById(long exerciseId){
+        Exercise exercise = null;
+        String[] cols = new String[]{"id", "name"};
+        String selection = "id = ?";
+        String[] selectionArgs = { String.valueOf(exerciseId) };
+
+        Cursor cursor = db.query("Exercise", cols, selection, selectionArgs,
+                null, null, null);
+
+        if(cursor.moveToFirst()) {
+            long id = cursor.getLong(cursor.getColumnIndexOrThrow("id"));
+            String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+            exercise = new Exercise(id, name);
+        }
+        cursor.close();
+        db.close();
+
+        return exercise;
+    }
+
+    public int updateExercise(Exercise exercise) {
+        long id = exercise.getId();
+        String name = exercise.getName();
+
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+
+        String whereClause = "id = ?";
+        String[] whereArgs = new String[]{ String.valueOf(id) };
+
+        return db.update("Exercise", values, whereClause, whereArgs);
+    }
+
+    public int deleteExercise(long exerciseId) {
+        String selection = "id = ?";
+        String[] selectionArgs = { String.valueOf(exerciseId) };
+
+        int deletedRows = db.delete("Exercise", selection, selectionArgs);
+
+        db.close();
+
+        return deletedRows;
     }
 }
